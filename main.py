de397c25004a044af6c5b8d07bfedc43c4f1395d
Matias Freire <matiaspfreire@gmail.com>
2026-04-23 10:17:22 -0400

    changes for dockerfile so it works

--- main.py
+++ main.py
@@ -473,4 +473,4 @@ register_display_values_callback(app)
 register_image_upload_callbacks(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8050, debug=False)
